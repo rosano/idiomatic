d032fb1c22b947e119e4543bd63c14f940787969
Rosano Coutinho <pub@xeari.com>
2020-12-10 16:42:46 -0300

    Rename mainModule

--- ui-logic-tests.js
+++ ui-logic-tests.js
@@ -1,11 +1,11 @@
 var assert = require('assert');
 
-var mainModule = require('./ui-logic.js');
+var mod = require('./ui-logic.js');
 
 describe('RCSIdiomaticLogicFormatMarkdown', function test_RCSIdiomaticLogicFormatMarkdown() {
 
 	it('returns constant', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicFormatMarkdown(), 'kRCSIdiomaticLogicFormatMarkdown');
+		assert.strictEqual(mod.RCSIdiomaticLogicFormatMarkdown(), 'kRCSIdiomaticLogicFormatMarkdown');
 	});
 
 });
@@ -13,7 +13,7 @@ describe('RCSIdiomaticLogicFormatMarkdown', function test_RCSIdiomaticLogicForma
 describe('RCSIdiomaticLogicFormatHTML', function test_RCSIdiomaticLogicFormatHTML() {
 
 	it('returns constant', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicFormatHTML(), 'kRCSIdiomaticLogicFormatHTML');
+		assert.strictEqual(mod.RCSIdiomaticLogicFormatHTML(), 'kRCSIdiomaticLogicFormatHTML');
 	});
 
 });
@@ -21,7 +21,7 @@ describe('RCSIdiomaticLogicFormatHTML', function test_RCSIdiomaticLogicFormatHTM
 describe('RCSIdiomaticLogicFormatRTF', function test_RCSIdiomaticLogicFormatRTF() {
 
 	it('returns constant', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicFormatRTF(), 'kRCSIdiomaticLogicFormatRTF');
+		assert.strictEqual(mod.RCSIdiomaticLogicFormatRTF(), 'kRCSIdiomaticLogicFormatRTF');
 	});
 
 });
@@ -29,10 +29,10 @@ describe('RCSIdiomaticLogicFormatRTF', function test_RCSIdiomaticLogicFormatRTF(
 describe('RCSIdiomaticLogicFormatsArray', function test_RCSIdiomaticLogicFormatsArray() {
 
 	it('returns constant', function () {
-		assert.deepEqual(mainModule.RCSIdiomaticLogicFormatsArray(), [
-			mainModule.RCSIdiomaticLogicFormatMarkdown(),
-			mainModule.RCSIdiomaticLogicFormatHTML(),
-			mainModule.RCSIdiomaticLogicFormatRTF()
+		assert.deepEqual(mod.RCSIdiomaticLogicFormatsArray(), [
+			mod.RCSIdiomaticLogicFormatMarkdown(),
+			mod.RCSIdiomaticLogicFormatHTML(),
+			mod.RCSIdiomaticLogicFormatRTF()
 		]);
 	});
 
@@ -42,16 +42,16 @@ describe('RCSIdiomaticLogicHTMLForMarkdown', function test_RCSIdiomaticLogicHTML
 
 	it('throws if not string', function () {
 		assert.throws(function () {
-			mainModule.RCSIdiomaticLogicHTMLForMarkdown(null);
+			mod.RCSIdiomaticLogicHTMLForMarkdown(null);
 		}, /RCSErrorInputNotValid/);
 	});
 
 	it('returns string', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicHTMLForMarkdown('alfa'), '<p>alfa</p>');
+		assert.strictEqual(mod.RCSIdiomaticLogicHTMLForMarkdown('alfa'), '<p>alfa</p>');
 	});
 
 	it('returns html if markdown', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicHTMLForMarkdown('_alfa_'), '<p><em>alfa</em></p>');
+		assert.strictEqual(mod.RCSIdiomaticLogicHTMLForMarkdown('_alfa_'), '<p><em>alfa</em></p>');
 	});
 
 });
@@ -60,24 +60,24 @@ describe('RCSIdiomaticLogicMarkdownForHTML', function test_RCSIdiomaticLogicMark
 
 	it('throws if not string', function () {
 		assert.throws(function () {
-			mainModule.RCSIdiomaticLogicMarkdownForHTML(null);
+			mod.RCSIdiomaticLogicMarkdownForHTML(null);
 		}, /RCSErrorInputNotValid/);
 	});
 
 	it('returns string', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicMarkdownForHTML('alfa'), 'alfa');
+		assert.strictEqual(mod.RCSIdiomaticLogicMarkdownForHTML('alfa'), 'alfa');
 	});
 
 	it('returns markdown if html', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicMarkdownForHTML('<em>alfa</em>'), '_alfa_');
+		assert.strictEqual(mod.RCSIdiomaticLogicMarkdownForHTML('<em>alfa</em>'), '_alfa_');
 	});
 
 	it('uses pound heading style', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicMarkdownForHTML('<h1>alfa</h1>'), '# alfa');
+		assert.strictEqual(mod.RCSIdiomaticLogicMarkdownForHTML('<h1>alfa</h1>'), '# alfa');
 	});
 
 	it('uses hyphen bullet style', function () {
-		assert.strictEqual(mainModule.RCSIdiomaticLogicMarkdownForHTML('<ul><li>alfa</li></ul>'), '-   alfa');
+		assert.strictEqual(mod.RCSIdiomaticLogicMarkdownForHTML('<ul><li>alfa</li></ul>'), '-   alfa');
 	});
 
 });
